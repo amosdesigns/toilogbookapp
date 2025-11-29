0898b333ef24453d9b8192eded279189430d8475
amosdesigns <amosdesigns@users.noreply.github.com>
2025-11-29 16:20:35 -0500

    docs updates and updated prisma

--- app/(admin)/dashboard/logs/page.tsx
+++ app/(admin)/dashboard/logs/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const LogManagementPage = () => {
+  // this is a book logbook management page for admin dashboard by location.
   //the logs Management page for admin dashboard only accessible by supervisor users or higher to view system logs or activity logs.
   //This is a placeholder for now. You can expand it later with actual log functionalities where you can view, filter, and manage logs.
   // from this page we should be able to create a incident report based on log data and start the reporting of this incident to relevant authorities roles. this workflow  start with the log data and the guard. next would be the supervisor review and finally the admin or higher authority to send the report to relevant authorities. at any point the report can be saved as draft or sent for review or final submission and  should be able to tracked and printed as needed. also should have a notification system to alert relevant users when a log is created or an incident report is generated.
